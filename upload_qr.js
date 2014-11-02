3393e39e81046f45908b0aeceaf54d9bee4fe01c
Philip Hayes <philiphayes9@gmail.com>
2014-11-02 01:17:11 -0700

    [Philip] Add QR Code uploading technologies!

---
+++ upload_qr.js
@@ -0,0 +1,13 @@
+var cloudinary = require('cloudinary');
+var qr = reqiure('qr-image');
+
+var create_send_qr = function (btc_addr, callback) {
+  var link = 'bitcoin://' + btc_addr + '?amount=0';
+  console.log('Creating QR Code with link', link);
+  var qr_png = qr.image(link, { type: 'png' });
+  var upload_stream = cloudinary.uploader.upload_stream(function (res) {
+    console.log('Finished uploading qr code', res.url);
+    callback(res.url);
+  });
+  qr_png.pipe(upload_stream);
+};
