e9c1287c70aa167a52c0a3e467f23dc74f6e8fdc
Jiahang Li <jiahang_li@outlook.com>
2014-11-01 17:12:39 -0700

    [Tak] Rename app

--- model.js
+++ model.js
@@ -1,7 +1,41 @@
 var mongoose = require("mongoose");
-var API_Code = 
+var API_Code = "";
 
-var createWallet = function() {
+// Schema and Model
+var accountSchema = mongoose.Schema({
+  guid: String,
+  address: String,
+  password: String,
+  phone: String
+});
+
+var accountModel = mongoose.model("accounts", accountSchema);
+
+// Database Connection
+var connected = false;
+var connectToMongoDB = function(callback, obj, next) {
+    if (!connected) {
+        var path = process.env.MONGOHQ_URL + "/";
+//        var path = "mongodb://localhost/wepay";
+        console.log("Try Connecting %s", path);
+        mongoose.connect(path);
+        mongoose.connection.on("error", function() {
+            console.error.bind("[Model] Connection Failed: ");
+            connected = false;
+        });
+        mongoose.connection.once("open", function() {
+            console.log("[Model] Connection Success!!! PATH: %s", path);
+            connected = true;
+            if (callback) {
+                callback(obj, next);
+            } else {
+                console.error("[Model] No Callback Specified");
+            }
+        });
+    }
+};
+
+var createWallet = function(password) {
 
 };
 
