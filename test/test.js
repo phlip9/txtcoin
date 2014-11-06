a5b5846f6ec1f6ce6071b7fdc89ec4a497a33a83
Philip Hayes <philiphayes9@gmail.com>
2014-11-06 00:04:42 -0800

    [Philip] Cleanup testing directory

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-describe('Math', function () {
-  describe('#max', function () {
-    it('returns the largest number from the arguments', function () {
-      expect(Math.max(1, 2, 3, 2)).to.equal(3);
-    });
-  });
-});
